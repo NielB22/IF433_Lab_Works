6edd10e4d0c91cc1ba775ac686077fe6c7f3d8db
NielB22 <danielarma321@gmail.com>
2026-02-03 15:29:22 +0700

    week1: refactor to use val and string templates

--- src/oop_00000113459_MuhammadDanielAzmi/week1/GradingSystem.kt
+++ src/oop_00000113459_MuhammadDanielAzmi/week1/GradingSystem.kt
@@ -1,9 +1,9 @@
 package oop_00000113459_MuhammadDanielAzmi.week1
 
 fun main() {
-    var name: String = "Daniel"
-    var score: Int = 80
+    val name: String = "Daniel"
+    val score: Int = 80
 
-    // Concatenation gaya lama
-    println("Nama: " + name + ", Nilai: " + score)
+    // REFACTOR: String Template ($name)
+    println("Nama: $name, Nilai: $score")
 }
