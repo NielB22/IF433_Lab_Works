f62add8d4fbccbc3dee7f05f9d3f857a62e6301c
NielB22 <danielarma321@gmail.com>
2026-02-03 15:26:24 +0700

    initial setup and procedural code

---
+++ src/oop_00000113459_MuhammadDanielAzmi/week1/GradingSystem.kt
@@ -0,0 +1,9 @@
+package oop_00000113459_MuhammadDanielAzmi.week1
+
+fun main() {
+    var name: String = "Daniel"
+    var score: Int = 80
+
+    // Concatenation gaya lama
+    println("Nama: " + name + ", Nilai: " + score)
+}
