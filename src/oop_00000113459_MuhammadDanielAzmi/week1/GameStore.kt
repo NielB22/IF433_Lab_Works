f085d6df82f29b25f96b488f1e7053950617abe6
NielB22 <danielarma321@gmail.com>
2026-02-05 07:23:21 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_00000113459_MuhammadDanielAzmi/week1/GameStore.kt
+++ src/oop_00000113459_MuhammadDanielAzmi/week1/GameStore.kt
@@ -2,6 +2,10 @@ package oop_00000113459_MuhammadDanielAzmi.week1
 
 fun main() {
 
-    val gameTitle: String
-    val price: Int
+    val gameTitle: String = "The Last Of Us Part2"
+    val price: Int = 600000
 }
+
+fun calculateDiscount(price: Int) =
+    if (price > 500000) price - (price * 20/100)
+    else price - (price * 10/100)
