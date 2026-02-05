1bb5c47c5bab9dc5564f97fb0f3b7bdfd498918d
NielB22 <danielarma321@gmail.com>
2026-02-05 07:36:40 +0700

    week01: (tugas 2) implement receipt printing

--- src/oop_00000113459_MuhammadDanielAzmi/week1/GameStore.kt
+++ src/oop_00000113459_MuhammadDanielAzmi/week1/GameStore.kt
@@ -2,10 +2,17 @@ package oop_00000113459_MuhammadDanielAzmi.week1
 
 fun main() {
 
-    val gameTitle: String = "The Last Of Us Part2"
-    val price: Int = 600000
+    val gameTitle: String
+    val price: Int
+
+    printReceipt(title = "The Last Of Us Part 2", finalPrice = 600000)
 }
 
 fun calculateDiscount(price: Int) =
     if (price > 500000) price - (price * 20/100)
     else price - (price * 10/100)
+
+fun printReceipt(title: String, finalPrice: Int){
+    val afterDiscount = calculateDiscount(finalPrice)
+    println("Game: $title | Harga: $afterDiscount")
+}
