4f524087a3a0f5ebce5182527d47c0a425c0f801
NielB22 <danielarma321@gmail.com>
2026-02-05 07:45:13 +0700

    week01: (tugas 2) null safety for user note

--- src/oop_00000113459_MuhammadDanielAzmi/week1/GameStore.kt
+++ src/oop_00000113459_MuhammadDanielAzmi/week1/GameStore.kt
@@ -4,15 +4,17 @@ fun main() {
 
     val gameTitle: String
     val price: Int
+    val userNote: String? = null
 
-    printReceipt(title = "The Last Of Us Part 2", finalPrice = 600000)
+    printReceipt(title = "The Last Of Us Part 2", finalPrice = 600000, userNote = userNote)
 }
 
 fun calculateDiscount(price: Int) =
     if (price > 500000) price - (price * 20/100)
     else price - (price * 10/100)
 
-fun printReceipt(title: String, finalPrice: Int){
+fun printReceipt(title: String, finalPrice: Int, userNote: String?){
     val afterDiscount = calculateDiscount(finalPrice)
     println("Game: $title | Harga: $afterDiscount")
+    println("Catatan: ${userNote ?: "Tidak ada catatan"}")
 }
