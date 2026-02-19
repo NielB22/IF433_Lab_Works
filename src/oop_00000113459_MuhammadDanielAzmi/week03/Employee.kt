f2e757a6f3e8aa94e4391b84468e559a0601c1b5
NielB22 <danielarma321@gmail.com>
2026-02-19 10:53:08 +0700

    week3: test all encapsulation logic in main

--- src/oop_00000113459_MuhammadDanielAzmi/week03/Employee.kt
+++ src/oop_00000113459_MuhammadDanielAzmi/week03/Employee.kt
@@ -1,6 +1,7 @@
 package oop_00000113459_MuhammadDanielAzmi.week03
 
 class Employee(val name: String){
+    val tax: Double get() = salary * 0.1
     var salary: Int = 0
         set(value) {
             if(value < 0){
