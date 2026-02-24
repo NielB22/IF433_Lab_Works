d0aca4bd701d9a3484ff7f0a0c94309ff3635092
NielB22 <danielarma321@gmail.com>
2026-02-24 17:10:52 +0700

    week3: (task1) implement weapon testing in main

---
+++ src/oop_00000113459_MuhammadDanielAzmi/week03/MainWeapon.kt
@@ -0,0 +1,10 @@
+package oop_00000113459_MuhammadDanielAzmi.week03
+
+fun main() {
+    var wpn = Weapon("Excalibur")
+    wpn.damage = 9999
+
+    println()
+    println("Tier: ${wpn.tier}")
+    println("Damage: ${wpn.damage}")
+}
