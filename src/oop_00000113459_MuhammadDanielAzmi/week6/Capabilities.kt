79635e8217ae00d784d6ed443bec8f47b03f0b52
NielB22 <danielarma321@gmail.com>
2026-03-10 15:12:26 +0700

    week6: create compound interfaces

---
+++ src/oop_00000113459_MuhammadDanielAzmi/week6/Capabilities.kt
@@ -0,0 +1,11 @@
+package oop_00000113459_MuhammadDanielAzmi.week6
+
+
+// Interface = Can-Do (Behavior)
+interface BluetoothConnectable {
+    fun connectToBluetooth()
+}
+
+interface Rechargeable {
+    fun chargeBattery()
+}
