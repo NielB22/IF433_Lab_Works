1f3c3facee7b96b11deef0d841f06db2f485d5c7
NielB22 <danielarma321@gmail.com>
2026-03-10 15:22:53 +0700

    week6: create Camera and Phone interfaces with default methods

---
+++ src/oop_00000113459_MuhammadDanielAzmi/week6/GadgetInterfaces.kt
@@ -0,0 +1,13 @@
+package oop_00000113459_MuhammadDanielAzmi.week6
+
+interface Camera {
+    fun turnOn() {
+        println("Lensa kamera terbuka dan sensor aktif.")
+    }
+}
+
+interface Phone {
+    fun turnOn() {
+        println("Sinyal seluler mencari jaringan.")
+    }
+}
