8d3e7f16dded9d23f8e204aa065a12c7c6ac1968
NielB22 <danielarma321@gmail.com>
2026-03-10 21:40:34 +0700

    week6: (task) test smart home system logic in main

---
+++ src/oop_00000113459_MuhammadDanielAzmi/week6/tugas/Main.kt
@@ -0,0 +1,25 @@
+package oop_00000113459_MuhammadDanielAzmi.week6.tugas
+
+fun main() {
+    val lamp = SmartLamp("1", "Ruang Tamu")
+    val speaker = SmartSpeaker("2", "Google Nest Dapur")
+    val cctv = SmartCCTV("3", "Ezviz Garasi")
+
+    println("\n============== SMART HOME ==============")
+    println("SmartLamp    : ${lamp.id} - ${lamp.name}")
+    println("SmartSpeaker : ${speaker.id} - ${speaker.name}")
+    println("SmartCCTV    : ${cctv.id} - ${cctv.name}")
+
+    val hub = SmartHomeHub()
+
+    hub.addDevice(lamp)
+    hub.addDevice(speaker)
+    hub.addDevice(cctv)
+
+    println("\n====== SECURITY MODE ACTIVATED =======")
+    hub.activateSecurityMode()
+
+
+    println("\n======== TURN OFF ALL SWITCH =========")
+    hub.turnOffAllSwitches()
+}
