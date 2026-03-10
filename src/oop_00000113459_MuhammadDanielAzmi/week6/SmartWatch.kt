04938e0d63e84c8486a31de06a5bea6eaf453965
NielB22 <danielarma321@gmail.com>
2026-03-10 15:19:50 +0700

    week6: create Smartwatch with multiple implementations

---
+++ src/oop_00000113459_MuhammadDanielAzmi/week6/SmartWatch.kt
@@ -0,0 +1,16 @@
+package oop_00000113459_MuhammadDanielAzmi.week6
+
+class SmartWatch : Watch(), BluetoothConnectable, Rechargeable {
+
+    override fun showTime() {
+        println("Layar OLED menyala: 14:00 WIB")
+    }
+
+    override fun connectToBluetooth() {
+        println("Mencari perangkat HP di sekitar untuk pairing...")
+    }
+
+    override fun chargeBattery() {
+        println("Mengisi daya menggunakan charger magnetik 15W.")
+    }
+}
