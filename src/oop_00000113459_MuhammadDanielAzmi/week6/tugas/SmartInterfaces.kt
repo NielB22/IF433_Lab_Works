a821b8be7c78ea046015568c9f39ff8a76d38dea
NielB22 <danielarma321@gmail.com>
2026-03-10 20:43:52 +0700

    week6: (task) create smart home interfaces

---
+++ src/oop_00000113459_MuhammadDanielAzmi/week6/tugas/SmartInterfaces.kt
@@ -0,0 +1,23 @@
+package oop_00000113459_MuhammadDanielAzmi.week6.tugas
+
+
+interface SmartDevice {
+    val id: String
+    val name: String
+}
+
+
+interface Switchable {
+    fun turnOn()
+    fun turnOff()
+}
+
+
+interface Recordable {
+
+    fun startRecord()
+
+    fun stopRecord() {
+        println("Perekaman dihentikan dan disimpan ke Cloud.")
+    }
+}
