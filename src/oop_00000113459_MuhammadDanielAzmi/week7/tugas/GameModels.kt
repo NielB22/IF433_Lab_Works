7a69a1844461e3e0db81820f984cdbfb4ab26172
NielB22 <danielarma321@gmail.com>
2026-03-20 11:00:26 +0700

    week7: create ItemRarity enum

---
+++ src/oop_00000113459_MuhammadDanielAzmi/week7/tugas/GameModels.kt
@@ -0,0 +1,9 @@
+package oop_00000113459_MuhammadDanielAzmi.week7.tugas
+
+enum class ItemRarity(val dropChance: Int) {
+    COMMON(70),
+    UNCOMMON(20),
+    RARE(7),
+    EPIC(2),
+    LEGENDARY(1)
+}
