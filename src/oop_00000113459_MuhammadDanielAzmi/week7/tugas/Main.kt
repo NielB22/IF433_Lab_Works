c34429565c19b1bd50f6f2854feb52dc4de8e4eb
NielB22 <danielarma321@gmail.com>
2026-03-20 14:18:01 +0700

    week7: (task) simulate rarity and factory instantiation

--- src/oop_00000113459_MuhammadDanielAzmi/week7/tugas/Main.kt
+++ src/oop_00000113459_MuhammadDanielAzmi/week7/tugas/Main.kt
@@ -2,6 +2,16 @@ package oop_00000113459_MuhammadDanielAzmi.week7.tugas
 
 fun main() {
 
+    println("\n==== MINI GAMES OOP ==== ")
     GameManager.startGame()
     GameManager.startGame()
+
+    var rarityL = ItemRarity.LEGENDARY
+    println("Drop chance Legendary : ${rarityL.dropChance}%")
+
+    var weaponS = Weapon.forgeStarterSword()
+    println("Name       : ${weaponS.item.name}")
+    println("Damage     : ${weaponS.item.damage}")
+    println("Rarity     : ${weaponS.item.rarity}")
+    println("Durability : ${weaponS.durability}")
 }
