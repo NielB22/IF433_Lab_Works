2dfef58920a4ad81299e160b405886c5099d4453
NielB22 <danielarma321@gmail.com>
2026-04-15 09:47:28 +0700

    week8: create LegacyJavaAPI class in Java

---
+++ src/oop_00000113459_MuhammadDanielAzmi/week8/LegacyJavaAPI.java
@@ -0,0 +1,8 @@
+package oop_00000113459_MuhammadDanielAzmi.week8;
+
+public class LegacyJavaAPI {
+    // fungsi ini ditulis dalam java. kotlin tidak tahu apakah kembaliannya nullable.
+    public static String fetchServerStatus() {
+        return "SERVER_ONLINE_200_OK";
+    }
+}
