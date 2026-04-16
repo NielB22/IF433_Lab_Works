abbb42835fd2e5a46f79b499b3df9f2608439eff
NielB22 <danielarma321@gmail.com>
2026-04-16 09:05:31 +0700

    week8: (task) process mixed raw JSON list using as?

--- src/oop_00000113459_MuhammadDanielAzmi/week8/ApiParser.kt
+++ src/oop_00000113459_MuhammadDanielAzmi/week8/ApiParser.kt
@@ -15,12 +15,12 @@ class ApiParser {
         val type = rawJson["type"] as? String
 
         return when (type) {
-            "Electronic" -> {
+            "ELECTRONIC" -> {
                 val warranty = (rawJson["warrantyMonths"] as? Int) ?: 12
                 Product.Electronic(id, name, warranty)
             }
 
-            "Clothing" -> {
+            "CLOTHING" -> {
                 val size = (rawJson["size"] as? String) ?: "All Size"
                 Product.Clothing(id, name, size)
             }
