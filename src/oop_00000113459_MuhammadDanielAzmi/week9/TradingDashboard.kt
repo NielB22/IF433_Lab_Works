743ab75ed7be3c6c8752990981e0654e2fdd4ef5
NielB22 <danielarma321@gmail.com>
2026-04-25 13:09:15 +0700

    week09: (task) populate mock trade history list

---
+++ src/oop_00000113459_MuhammadDanielAzmi/week9/TradingDashboard.kt
@@ -0,0 +1,13 @@
+package oop_00000113459_MuhammadDanielAzmi.week9
+
+fun main() {
+    val tradehistory: List<TradeLog> = listOf(
+        TradeLog("BTCUSDT", "LONG", 10, 7.3, "OPEN"),
+        TradeLog(pair = "BTCUSDT", position = "LONG",  leverage = 10, roe =  8.5,  status = "CLOSED"),
+        TradeLog(pair = "BTCUSDT", position = "SHORT", leverage = 5,  roe = -3.2,  status = "CLOSED"),
+        TradeLog(pair = "ETHUSDT", position = "LONG",  leverage = 20, roe =  12.7, status = "CLOSED"),
+        TradeLog(pair = "ETHUSDT", position = "SHORT", leverage = 10, roe = -6.1,  status = "CLOSED"),
+        TradeLog(pair = "BTCUSDT", position = "LONG",  leverage = 15, roe =  4.3,  status = "OPEN"),
+        TradeLog(pair = "SOLUSDT", position = "SHORT", leverage = 8,  roe = -1.5,  status = "OPEN"),
+    )
+}
