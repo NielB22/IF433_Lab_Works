336b59b5fa3c79e06fcf17e13581be521dac9d64
NielB22 <danielarma321@gmail.com>
2026-04-28 16:00:16 +0700

    week10: (task) populate coin repository

--- src/oop_00000113459_MuhammadDanielAzmi/week10/CryptoDashboard.kt
+++ src/oop_00000113459_MuhammadDanielAzmi/week10/CryptoDashboard.kt
@@ -1,4 +1,10 @@
 package oop_00000113459_MuhammadDanielAzmi.week10
 
-class CryptoDashboard {
+fun main() {
+    val coinRepo = WalletRepository<Coin>()
+
+    // Lanjutan di dalam main()
+    coinRepo.add(Coin("BTC", 0.5))
+    coinRepo.add(Coin("ETH", 2.0))
+    coinRepo.add(Coin("USDT", 100.0))
 }
