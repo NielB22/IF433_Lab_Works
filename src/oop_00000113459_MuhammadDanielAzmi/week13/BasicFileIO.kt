524f93c5d75793704983bf8dd0ed531b6cd504c3
NielB22 <danielarma321@gmail.com>
2026-05-19 18:02:24 +0700

    week13: implement appendText & readLines

--- src/oop_00000113459_MuhammadDanielAzmi/week13/BasicFileIO.kt
+++ src/oop_00000113459_MuhammadDanielAzmi/week13/BasicFileIO.kt
@@ -6,4 +6,8 @@ fun main() {
     val file = File("src/oop_00000113459_MuhammadDanielAzmi/week13/notes.txt")
     file.writeText("Line 1: Inisialisasi sistem. \n")
     println("File berhasil dibuat dan ditulis")
+
+    file.appendText("Line 2: Menambahkan konfigurasi baru.\n")
+    println("Text berhasil di-append.")
 }
+
