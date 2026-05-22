e0cbafebad004dfb426ecda849eae255ca145af2
NielB22 <danielarma321@gmail.com>
2026-05-22 07:54:53 +0700

    week13: create Student model

---
+++ src/oop_00000113459_MuhammadDanielAzmi/week13/StudentSerialization.kt
@@ -0,0 +1,7 @@
+package oop_00000113459_MuhammadDanielAzmi.week13
+
+import java.io.File
+import java.io.FileNotFoundException
+
+// --- LANGKAH 1: Struktur Data Class ---
+data class Student(val name: String, val age: Int, val gpa: Double)
