23d66015f7723f2d05dca963e3ae8245e7945310
NielB22 <danielarma321@gmail.com>
2026-05-26 15:02:03 +0700

    week14: create DiscountStrategy interface and implementations

---
+++ src/oop_00000113459_MuhammadDanielAzmi/week14/OcpRefactored.kt
@@ -0,0 +1,21 @@
+package oop_00000113459_MuhammadDanielAzmi.week14
+
+interface DiscountStrategy {
+    fun apply(price: Double): Double
+}
+
+class StudentDiscount : DiscountStrategy {
+    override fun apply(price: Double) = price * 0.80
+}
+
+class MemberDiscount : DiscountStrategy {
+    override fun apply(price: Double) = price * 0.85
+}
+
+class SafeDiscountCalculator(private val strategy: DiscountStrategy) {
+    fun calculate(price: Double) = strategy.apply(price)
+}
+
+class SeniorDiscount : DiscountStrategy {
+    override fun apply(price: Double) = price * 0.75
+}
